4ae10ba29b169bd09dc90f22b747fc723d938a01
Mr. JT Nyika <u19107430@up.ac.za>
2020-03-10 14:31:51 +0200

    Hi my name is eminem

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load",function(){
+    document.getElementById("me").addEventListener("click",function(){
+        alert("I am excited for this module as it teaches me diferent programming languages");
+    })
+})
